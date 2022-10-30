b49d81ef32a2be13b2dc6c5f8e0432b47cad4a0f
zhayujie <yjzha1996@163.com>
2022-10-30 23:10:19 +0800

    day2_LC171_yujie commit

---
+++ day2_LC171/TitleToNumber_yujie.java
@@ -0,0 +1,17 @@
+package day2_LC171;
+
+/**
+ * 171. Excel 表列序号
+ *
+ * @author zhayujie
+ * @date 2022/10/30
+ */
+public class TitleToNumber_yujie {
+    public int titleToNumber(String columnTitle) {
+        int sum = 0;
+        for (char ch : columnTitle.toCharArray()) {
+            sum = sum * 26 + ((ch - 'A') + 1);
+        }
+        return sum;
+    }
+}
