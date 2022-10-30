d7c0dc27e6543150080ff60785c87324c37287f0
yud11 <1969527445@qq.com>
2022-10-30 22:52:47 +0800

    yud1:day3题目和代码更新

---
+++ day2_LC171/README.md
@@ -0,0 +1,3 @@
+2022.10.31 简单题
+
+LeetCode 171.Excel 表列序号: https://leetcode.cn/problems/excel-sheet-column-number/
---
+++ day2_LC171/TitleToNumber_yud1.java
@@ -0,0 +1,16 @@
+/**
+ * @autor yud1
+ * @date 2022/10/30 22:30
+ */
+public class TitleToNumber_yud1 {
+
+    public  int titleToNumber(String columnTitle) {
+        if(columnTitle == null || columnTitle.length() == 0) return 0;
+        int len = columnTitle.length();
+        int sum = 0 ;
+        for (int i = 0; i < len; i++) {
+            sum = sum * 26 +  (columnTitle.charAt(i) - 'A' + 1);
+        }
+        return sum;
+    }
+}
