e1929c7f46ffb9c3947f923700d1e4233004baeb
yud11 <1969527445@qq.com>
2022-11-01 23:03:21 +0800

    yud1:更新day4题目

---
+++ day3_LC257/BinaryTreePaths_yud1.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @autor yud1
+ * @date 2022/10/31 19:45
+ */
+public class BinaryTreePaths_yud1 {
+
+    List<String> list = null;
+    public List<String> binaryTreePaths(TreeNode root) {
+        list = new ArrayList<>();
+        if (root == null) return list;
+        binaryTreePaths(root,"");
+        return list;
+    }
+
+
+    public void binaryTreePaths(TreeNode root, String val) {
+        if (val != ""){
+            val = val + "->";
+        }
+        val += root.val;
+        if(root.left == null && root.right == null){
+            list.add(val);
+            return;
+        }
+        if (root.left!=null){
+            binaryTreePaths(root.left,val);
+        }
+
+        if (root.right!=null){
+            binaryTreePaths(root.right, val);
+        }
+    }
+}
---
+++ day4_LC121/README.md
@@ -0,0 +1,3 @@
+2022.11.2 简单题
+
+LeetCode 121:买卖股票的最佳时机:https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
