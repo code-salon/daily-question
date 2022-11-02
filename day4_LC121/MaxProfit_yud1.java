cb5595b56fec3dadd9a7b8dd0ab47049b22cf6e4
yud11 <1969527445@qq.com>
2022-11-02 14:49:40 +0800

    yud1:day4代码上传

---
+++ day4_LC121/MaxProfit_yud1.java
@@ -0,0 +1,20 @@
+/**
+ * @autor yud1
+ * @date 2022/11/2 14:19
+ */
+public class MaxProfit_yud1 {
+
+    public int maxProfit(int[] prices) {
+        if (prices.length <= 1) return 0;
+        //卖出最大价格
+        int maxPrice = 0;
+        //最低价格
+        int min = prices[0];
+        for (int i=1; i< prices.length;i++){
+            //此时的min表示前i-1天的最低价格，prices[i] - min表示在第i天卖出去的最大收益
+            maxPrice = Math.max(maxPrice,prices[i] - min);
+            min = Math.min(min,prices[i]);
+        }
+        return maxPrice;
+    }
+}
