968c4cedad68b11af9bddc3625f3942b6c3741d6
wangjinfeng <wangjinfeng@fosunhealth.com>
2022-11-07 17:28:55 +0800

    寻找数组的中心下标

---
+++ day6_LC724/PivotIndex_wjf.java
@@ -0,0 +1,24 @@
+package day6_LC724;
+
+import java.util.Arrays;
+
+/**
+ * @Classname PivotIndex_wjf
+ * @Description
+ * @Date 2022/11/7 17:24
+ * @Created by wangjinfeng
+ */
+public class PivotIndex_wjf {
+
+    public int pivotIndex(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int leftSum = 0;
+        for (int i=0;i<nums.length;i++) {
+            if (leftSum*2+nums[i]==sum) {
+                return i;
+            }
+            leftSum+=nums[i];
+        }
+        return -1;
+    }
+}
