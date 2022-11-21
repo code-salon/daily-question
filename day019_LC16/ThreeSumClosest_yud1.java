84b856cae5fe24e6d29ce3a980c0454994b33fe4
yud11 <1969527445@qq.com>
2022-11-21 09:24:18 +0800

    yud1:day19

--- day019_LC16/ThreeSumClosest_yud1.java
+++ day019_LC16/ThreeSumClosest_yud1.java
@@ -8,15 +8,39 @@ import java.util.Arrays;
  */
 public class ThreeSumClosest_yud1 {
 
-/**    public int threeSumClosest(int[] nums, int target) {
+    public int threeSumClosest(int[] nums, int target) {
 
         Arrays.sort(nums);
 
         int n = nums.length;
+        int minClosest = Integer.MAX_VALUE;
+        int minSum = 0;
 
         for (int i = 0; i < n - 2; i++) {
             int left = i + 1;
             int right = n - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right] + nums[i];
+                if (Math.abs(sum-target) < minClosest){
+                    minClosest = Math.abs(sum-target);
+                    minSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    return sum;
+                }
+            }
         }
-    }*/
+        return minSum;
+    }
+
+
+    public static void main(String[] args) {
+        ThreeSumClosest_yud1 main = new ThreeSumClosest_yud1();
+        System.out.println(main.threeSumClosest(new int[]{-1,2,1,-4},1));
+    }
 }
